0a006e49d3f35317ec69e218bdb8e8d1b9894ab8
Steven <vduong.steven@gmail.com>
2025-03-14 16:21:11 -0700

    Theme and some minor filtering for interesting amenities

---
+++ main.py
@@ -0,0 +1,46 @@
+# CMPT 353 Project
+#
+#   Steven Duong 301552606
+#   
+#
+
+# Notes:
+#   if you want to see the entire tag line write 
+#   print(data['tags'].iloc[0]) 
+#   replace 0 with different row numbers
+
+import pandas as pd
+import numpy as np
+import folium as fl
+
+def main():
+    data = pd.read_json("amenities-vancouver.json.gz", compression="gzip", lines=True)
+    
+    # Different Themes of amenities
+    themes = {
+        "Nature": ["park", "watering_place", "fountain", "ranger_station", "hunting_stand", "observation_platform"],
+        "Food": ["cafe", "fast_food", "bbq", "restaurant", "pub", "bar", "food_court", "ice_cream", "juice_bar", "bistro", "biergarten"],
+        "History": ["place_of_worship", "monastery", "courthouse", "townhall"],
+        "Science": ["research_institute", "science", "healthcare", "hospital", "pharmacy", "clinic", "veterinary", "chiropractor", "ATLAS_clean_room"],
+        "Art": ["arts_centre", "theatre", "studio", "music_school"],
+        "Entertainment": ["cinema", "nightclub", "stripclub", "gambling", "casino", "events_venue", "marketplace", "spa", "events_venue", "internet_cafe", "lounge", "shop|clothes", "leisure"],
+        "Mode of Travel": ["car_rental", "bicycle_rental", "car_sharing", "taxi", "bus_station", "ferry_terminal", "seaplane_terminal", "motorcycle_rental", "parking", "charging_station", "EVSE"]
+    }
+    
+    # Small filtering
+    data = data[~data['name'].isna()]
+    
+    # Data with tags that contain wikidata or wikipedia values and other things that may be useful
+    # Some places may not have wikidata or wikipedia, but may be interesting and contain other keys
+    interesting_data = data[data['tags'].apply(lambda x: 'brand:wikidata' in x or 
+                                               'brand:wikipedia' in x or 
+                                               'official_name' in x or
+                                               'opening_hours' in x or
+                                               'cuisine' in x or
+                                               'addr:street' in x or
+                                               'addr:housenumber' in x)]
+
+
+    
+if __name__=='__main__':
+    main()
